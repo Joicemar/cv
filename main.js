cec4d8468d810dd3312978ba88993e1f33c2d5c8
Joicemar <33008360+Joicemar@users.noreply.github.com>
2023-04-16 01:37:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+let typingText = new Typed("#text", {
+  strings: ["Joicemar", "Programador", "Desenvolvedor front-end"],
+  loop: true,
+  backDelay: 3000,
+  typeSpeed: 100,
+  backSpeed: 30,
+  loop: true,
+})
+
+const circle = document.querySelector(".dayNight");
+let body = document.querySelector(".body");
+
+
+let dayNight = document.querySelector("#box");
+dayNight.addEventListener('click', function () {
+    if (body.classList.contains("day")) {
+        body.classList.remove("day");
+        body.classList.add("night");
+    } else {
+        body.classList.remove("night");
+        body.classList.add("day");
+    }
+});
+
+let bars = document.querySelector(".bars");
+let menu = document.querySelector(".menu");
+
+bars.addEventListener('click', function () {
+
+    menu.classList.toggle('active');
+
+});
